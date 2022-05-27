54315862fd6bebe1d1beb66897d2a1b7b4e9db78
Jerry Hu <j376hu@uwaterloo.ca>
2022-05-27 19:32:50 -0400

    Revert "Add feature: Overload < operator for Product class"
    This reverts commit 8b5c4d9f88926b56db9c5865659d063905736b9f.

--- Product.hpp
+++ Product.hpp
@@ -17,10 +17,6 @@ public:
     //
     ~Product();
 
-    //Overload < operator
-	//
-	bool operator<( Product const &other);
-
     std::string get_name() const;
     std::string get_type() const;
     int get_buy_date() const;
